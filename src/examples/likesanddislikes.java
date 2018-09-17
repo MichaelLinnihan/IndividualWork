11756c6ad52e58452952c387a2cce0edb8513e0e
Unknown <mickedylinnihan@gmail.com>
2018-09-17 09:06:35 -0500

    likes and dislikes

---
+++ src/examples/likesanddislikes.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class likesanddislikes {
+	public static void main(String[] args) {
+		System.out.println("Likes \t \t Dislikes");
+		System.out.println("steak \t \t burgers");
+		System.out.println("launch \t \t trig");
+		System.out.println("games \t \t running");
+		System.out.println("water polo \t \t drills");
+		
+	
+	}
+
+}
