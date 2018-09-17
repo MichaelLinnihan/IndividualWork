26443cfac7498d2a5cc662d6045c1cbd3be615ef
Unknown <mickedylinnihan@gmail.com>
2018-09-17 08:55:33 -0500

    Schedule

---
+++ src/examples/Schedule.java
@@ -0,0 +1,14 @@
+package examples;
+
+public class Schedule {
+	public static void main(String[] args) {
+		System.out.println("Michael Linnihan");
+		System.out.println("comp sci");
+		System.out.println("pre calc");
+		System.out.println("trig");
+		System.out.println("physics");
+		System.out.println("A.P Micro");
+	
+	}
+
+}
