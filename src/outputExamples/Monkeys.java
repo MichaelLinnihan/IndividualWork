58f45ee352b103fc4bbc9cc5e2b5242f7012eb7f
Unknown <mickedylinnihan@gmail.com>
2018-09-25 08:17:29 -0500

    brokenbones

---
+++ src/outputExamples/Monkeys.java
@@ -0,0 +1,14 @@
+package outputExamples;
+import java.util.Scanner; 
+
+public class Monkeys {
+	public static void main(String [] args)
+	{
+	Scanner input = new Scanner(System.in);
+	//your program starts here
+	int height = input.nextInt();
+	System.out.println("the height of an adult monkey is " + height);
+	}
+
+
+}
