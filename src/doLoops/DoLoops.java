4b2bf317aaad2762dafa885fd2d074ad48b09073
b1zt_mcha31 <mickedylinnihan@gmail.com>
2018-11-12 09:00:46 -0600

    First  do Loops

--- src/doLoops/DoLoops.java
+++ src/doLoops/DoLoops.java
@@ -1,5 +1,26 @@
 package doLoops;
 
-public class DoLoops {
+import java.util.Scanner;
 
+public class DoLoops {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("give num");
+		int number = input.nextInt();
+		int smallest = number;
+		int largest = number;
+		int newNum = 0;
+		do {
+			System.out.println("type new number");
+			newNum = input.nextInt();
+			if (newNum > largest && newNum !=9999) {
+				largest = newNum;
+			}
+			if (newNum < smallest && newNum !=9999) {
+				smallest = newNum;
+			}
+		} while (newNum != 9999);
+		System.out.println(largest - smallest);
+	}
 }
